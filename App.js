453e6d55c8d04fab2f464083647aa810c23f15f7
JairoAguilar1803 <56201419+JairoAguilar1803@users.noreply.github.com>
2022-04-14 20:10:56 -0500

    agregue console log

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Navigatio from './src/navigation/Navigatio';
 import { StyleSheet, FlatList,ActivityIndicator,Platform} from 'react-native'
 export default function App() {
+  console.log("Este es un ejemplo");
   return (
     <NavigationContainer>
       <Navigatio/>
