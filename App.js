fffbc9e24c1a1d62ab2710b81ff5adddc0232c95
JairoAguilar1803 <56201419+JairoAguilar1803@users.noreply.github.com>
2022-04-14 20:07:24 -0500

    se agrego importación

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigatio from './src/navigation/Navigatio';
-
+import { StyleSheet, FlatList,ActivityIndicator,Platform} from 'react-native'
 export default function App() {
   return (
     <NavigationContainer>
